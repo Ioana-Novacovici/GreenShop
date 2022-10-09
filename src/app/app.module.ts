025505d83d75ea0cbce02b742893485ea4bbc631
Ioana-Novacovici <ioananovacovici@gmail.com>
2022-10-09 22:54:15 +0300

    initial state green-shop project with login and register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { FooterComponent } from './components/footer-bar/footer.component';
+import {NgbDropdown, NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import {RouterModule} from "@angular/router";
+import {AngularFireModule} from "@angular/fire/compat";
+import {environment} from "../environments/environment";
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    RouterModule.forRoot([
+      {path: 'home', component: AppComponent},
+      {path: 'register', component: RegisterComponent},
+      {path: 'login', component: LoginComponent}
+    ]),
+    AngularFireModule.initializeApp(environment.firebase),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'green-shop-2e695',
+    appId: '1:821309949475:web:bf93cdedbd4b48f9709396',
+    storageBucket: 'green-shop-2e695.appspot.com',
+    apiKey: 'AIzaSyCGrGmjv-DCNC3Anuw2sMeKUiIf9YnH06U',
+    authDomain: 'green-shop-2e695.firebaseapp.com',
+    messagingSenderId: '821309949475',
+    measurementId: 'G-C7LESQDMB4',
+  }
+
 };
 
 /*
